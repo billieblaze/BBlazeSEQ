6089d26febe38244898b9a56607ab4d0f92350e3
BillieBlaze <bill@socialcloudz.com>
2012-05-08 15:13:02 -0400

    removed util.h which came from cahors

--- bblazeSEQ.ino
+++ bblazeSEQ.ino
@@ -13,6 +13,33 @@ BBlazeSeq v0.5
 #define  noteoff_size 32   // size of the noteoff arrays in memory
 #define  mask         0x80 // used for drum patterns. b10000000
 
+// MIDI notes
+#define  C   0
+#define  Cs  1
+#define  D   2
+#define  Ds  3
+#define  E   4
+#define  F   5
+#define  Fs  6
+#define  G   7
+#define  Gs  8
+#define  A   9
+#define  As  10
+#define  B   11
+
+// Octaves
+#define  oct0  0
+#define  oct1  12
+#define  oct2  24
+#define  oct3  36
+#define  oct4  48
+#define  oct5  60
+#define  oct6  72
+#define  oct7  84
+
+
+
+
 
 String version = "0.5";
 
